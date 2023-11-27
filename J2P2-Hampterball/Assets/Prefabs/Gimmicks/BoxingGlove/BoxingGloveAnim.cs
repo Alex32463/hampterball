4082132d4a7e2c7b7db05312679a79e8a8afb90c
Fernando Kook <Fernandokook1023@gmail.com>
2023-11-27 09:31:29 +0100

    Feat: added Gimmicks

---
+++ J2P2-Hampterball/Assets/Prefabs/Gimmicks/BoxingGlove/BoxingGloveAnim.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class BoxingGloveAnim : MonoBehaviour
+{
+    Animator animator;
+    Ray ray;
+    RaycastHit hit;
+    [SerializeField] LayerMask hitMask;
+
+    [SerializeField] GameObject animatorGameObject;
+    void Start()
+    {
+        animator = animatorGameObject.gameObject.GetComponent<Animator>();
+    }
+    void Update()
+    {
+        // Creates the raycast
+        Physics.Raycast(transform.position, transform.forward, out hit, 3.2f, hitMask);
+
+        // Raycast checking if a player is hit
+        if (hit.collider != null)
+        {
+            animator.SetTrigger("raycast");
+        }
+        if (this.animator.GetCurrentAnimatorStateInfo(0).IsName("Armature|ArmatureAction"))
+        {
+            animator.ResetTrigger("raycast");
+        }
+    }
+}
