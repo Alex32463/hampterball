5ea5a558257ce35548c0bfec7e403b2ebdb33bd5
Fernando Kook <Fernandokook1023@gmail.com>
2023-12-04 14:05:34 +0100

    ADD: added comments to my code

--- J2P2-Hampterball/Assets/Prefabs/Interactive Level Prefabs/Trampoline/PlayAnimation.cs
+++ J2P2-Hampterball/Assets/Prefabs/Interactive Level Prefabs/Trampoline/PlayAnimation.cs
@@ -9,40 +9,42 @@ public class PlayAnimation : MonoBehaviour
     private Animator animator;
     public bool pressedE;
 
-    private Movement playerMovement; // Gets PlayerMovent script
+    private Movement playerMovement;
     private float bounceHeight = 200;
  
 
     void Start()
     {
         animator = GetComponent<Animator>();
-        playerMovement = FindObjectOfType<Movement>();
+        playerMovement = FindObjectOfType<Movement>(); // Gets PlayerMovent script \\
     }
 
     // Checks if the playes is collided with the collider of the bouncepad
     void OnTriggerStay(Collider collision)
     {
-        // (THEN) If the playes is collided with the collider of the bouncepad
+        // (THEN) If the playes is collided with the collider of the bouncepad AND the animation is not playing
         if (collision.CompareTag("Player") && !animator.GetCurrentAnimatorStateInfo(0).IsName("Armature|ArmatureAction"))
         {
-            animator.SetTrigger("E");
+            animator.SetTrigger("E"); // Starts the animation \\
             pressedE = true;
         }
         if (pressedE == true)
         {
-            playerMovement.rb.AddForce(transform.up * bounceHeight);
+            playerMovement.rb.AddForce(transform.up * bounceHeight); // adds force to the player in the playerMovement script to the ridgitbody \\
         }
+        // If the animation is playing reset the animation (name: "Armature|ArmatureAction")
         if (animator.GetCurrentAnimatorStateInfo(0).IsName("Armature|ArmatureAction"))
         {
-            animator.ResetTrigger("E");
+            animator.ResetTrigger("E"); // Resets animation trigget, so that it doesn't play animation twice \\
         }
     }
+    // If you leave the trampoline collision it plays the contents
     private void OnTriggerExit(Collider collision)
     {
-        // Checks if the playes is NOT collided with the collider of the bouncepad
+        // Checks if the playes is collided with the collider of the bouncepad or not
         if (collision.CompareTag("Player"))
         {
-            pressedE = false;
+            pressedE = false; 
         }
     }
 }
